9bf310de77ca46e368604c09ee9b635b01658320
Traorasp <miguelsolanopaz@outlook.com>
2022-05-14 14:19:13 -0400

    I structured and styled the book form, now form appears when add book button is pressed and is cleared and hidden when x or add book button is pressed

--- library.js
+++ library.js
@@ -1,24 +1,53 @@
 const library = document.querySelector('.library');
 
+const bookFormButton = document.querySelector('.controls button');
+const bookForm = document.querySelector('.form');
+bookFormButton.addEventListener('click', displayForm);
+
+const addBookButton = document.querySelector('.submit-form');
+addBookButton.addEventListener('click', addBookToLibrary);
+
+const exitButton = document.querySelector(".exit-form");
+exitButton.addEventListener('click', exitForm)
+
+
 let test = new Book('abc', 'abx', 34, true);
 let test2 = new Book('a1bc', 'abx', 344, true);
 
 
 let myLibrary = [];
 
-addBookToLibrary(test);
-addBookToLibrary(test2);
-displayBooks();
+function exitForm() {
+    bookForm.classList.add('hide');
+}
 
-function Book(author, title, numPages, read){
+function displayForm() {
+bookForm.classList.remove('hide');
+}
+
+function Book(title, author, numPages, read){
     this.author = author;
     this.title = title;
     this.numPages = numPages;
     this.read = read;
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+    let book = new Book(document.getElementById("title").value, 
+    document.getElementById("author").value,
+    document.getElementById("pages").value,
+    document.getElementById("read").value);
     myLibrary.push(book);
+    bookForm.classList.add('hide');
+    clearForm();
+    displayBooks();
+}
+tgt
+function clearForm(){
+    document.querySelector('#title').value = "";
+    document.querySelector('#author').value = "";
+    document.querySelector('#pages').value = "";
+    document.querySelector('#read').checked = false;
 }
 
 function displayBooks() {
@@ -34,8 +63,8 @@ function displayBooks() {
         bookNumPage.textContent = `Page # : ${book.numPages}pg`;
         let bookRead = document.createElement('p');
         bookRead.textContent = `Read: ${book.read}`;
-        bookBody.appendChild(bookAuthor)
         bookBody.appendChild(bookTitle)
+        bookBody.appendChild(bookAuthor)
         bookBody.appendChild(bookNumPage)
         bookBody.appendChild(bookRead)
         library.appendChild(bookBody);
